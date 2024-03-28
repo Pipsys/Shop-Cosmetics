19e4bc524e7da7f737b3c74026f92d31155b9db8
Igor Davidov <igordavidovsteam@gmail.com>
2024-03-28 18:08:20 +0300

    db json creat

--- Shop-Cosmetics/Shop-Cosmetics/Data/AppDBContent.cs
+++ Shop-Cosmetics/Shop-Cosmetics/Data/AppDBContent.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;
+using Shop_Cosmetics.Data.Models;
 
 
 namespace Shop_Cosmetics.Data
 {
 	public class AppDBContent : DbContext
 	{
-		
+		public AppDBContent(DbContextOptions<AppDBContent> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Cosmetics> Cosmetics { get; set; }
+		public DbSet<Category> Category { get; set; }
 	}
 }
 
